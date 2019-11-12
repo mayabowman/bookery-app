02af26846cd58fd140c7a34898a00577ee7fba03
Maya Bowman <mayashady1@gmail.com>
2019-11-12 11:01:15 -0500

    created landing page

---
+++ src/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <main className="LandingPage">
+      <h1>Welcome to Bookery</h1>
+      <p>Every book is a journey. Let Bookery help you choose your next adventure, 
+        keep track of your reads, and rate your experiences! Click below to get started.
+      </p>
+    </main>
+  );
+}
+
+export default LandingPage

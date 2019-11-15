4446f08f193676973609ee1d203fed7cd9c7caa3
Maya Bowman <mayashady1@gmail.com>
2019-11-15 18:08:17 -0500

    worked on bookshelf

--- src/MyBookshelf/MyBookshelf.js
+++ src/MyBookshelf/MyBookshelf.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 function MyBookshelf(props) {
   console.log("bookshelf", props.bookshelf)
+  // bookshelf is an array, access values and match to books from state
+  // render book component for each book in array
   return (
      <div>
       <h1>Your Bookshelf</h1>

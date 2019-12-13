5e74280177af283e3f3e136bc773b9983b90273d
Maya Bowman <mayashady1@gmail.com>
2019-12-13 12:12:34 -0500

    working on cors

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api",
-  // API_ENDPOINT: "https://stormy-cliffs-13063.herokuapp.com/api",
+  // API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://stormy-cliffs-13063.herokuapp.com/api",
   TOKEN_KEY: "bookery-client-auth-token"
 }

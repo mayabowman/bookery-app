2e0544e1944d715346f2765d83d729fa5b16af19
Maya Bowman <mayashady1@gmail.com>
2020-01-13 14:37:46 -0500

    working on issues with adding and deleting books

--- src/App/App.js
+++ src/App/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
       sideDrawerOpen: false,
     }
     this.drawerToggleClickHandler = this.drawerToggleClickHandler.bind(this)
+    this.handleRemoveBook = this.handleRemoveBook.bind(this)
   }
 
   static contextType = AppContext;
@@ -81,6 +82,15 @@ class App extends React.Component {
     if (this.state.bookshelf.includes(id)) {
       alert('This is already on your bookshelf!')
     }
+  };
+
+  handleRemoveBook = (id) => {
+    BookshelfApiService.deleteBookshelfItem(id)
+    let array = [...this.state.bookshelf]
+    let updatedBookshelf = array.filter(bookshelfItem => {
+      return bookshelfItem.id !== id
+    })
+    this.setState({ bookshelf: updatedBookshelf })
   };
 
   render() {
@@ -158,3 +168,186 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { Route, Switch } from 'react-router-dom';
+// import LandingPage from '../LandingPage/LandingPage';
+// import SignUp from '../SignUp/SignUp';
+// import LogIn from '../LogIn/LogIn';
+// import BrowseBooks from '../BrowseBooks/BrowseBooks';
+// import Bookshelf from '../Bookshelf/Bookshelf';
+// import ReviewForm from '../ReviewForm/ReviewForm';
+// import BookshelfApiService from '../services/bookshelf-api-service';
+// import AppContext from '../contexts/AppContext';
+// import config from '../config';
+// import './App.css';
+// import Nav from '../Nav/Nav';
+// import SideDrawer from '../SideDrawer/SideDrawer';
+// import Backdrop from '../Backdrop/Backdrop';
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       books: [],
+//       bookshelf: [],
+//       error: null,
+//       sideDrawerOpen: false,
+//     }
+//     this.drawerToggleClickHandler = this.drawerToggleClickHandler.bind(this)
+//   }
+
+//   static contextType = AppContext;
+
+//   drawerToggleClickHandler = () => {
+//     this.setState((prevState) => {
+//       return {sideDrawerOpen: !prevState.sideDrawerOpen}
+//     })
+//   };
+
+//   backdropClickHandler = () => {
+//     this.setState({ sideDrawerOpen: false })
+//   };
+
+//   componentDidMount() {
+//     fetch(`${config.API_ENDPOINT}/books`)
+//       .then((booksRes) => {
+//         if (!booksRes.ok) {
+//           throw new Error(booksRes.statusText)
+//         }
+//         return booksRes.json()
+//       })
+//       .then(data => {
+//         this.setState({
+//           books: data,
+//           error: null
+//         })
+//       })
+//       .catch(err => {
+//         this.setState({
+//           error: 'Sorry, could not get books at this time.'
+//         })
+//       })
+//   };
+
+//   handleAddToBookshelf = (id) => {
+//     this.setState({
+//       bookshelf: !this.state.bookshelf.includes(id)
+//                   ? [...this.state.bookshelf, id]
+//                   : this.state.bookshelf
+//     })
+//     if (!this.state.bookshelf.includes(id)) {
+//       BookshelfApiService.postBookshelfItem(id, 'dummy text', '3')
+//         .then((data) =>{
+//           if (id !== data.book_id) {
+//             this.setState({
+//               bookshelf: [...this.state.bookshelf, data.book_id]
+//             })
+//           }
+//         })
+//         .catch(err => {
+//           console.log('error', err)
+//         })
+//     }
+//     if (this.state.bookshelf.includes(id)) {
+//       alert('This is already on your bookshelf!')
+//     }
+//   };
+
+//   render() {
+//     const contextValue = {
+//       handleAddToBookshelf: this.handleAddToBookshelf,
+//       books: this.state.books,
+//       bookshelf: this.state.bookshelf
+//     };
+
+//     let { books } = this.state;
+//     let backdrop;
+
+//     if (this.state.sideDrawerOpen) {
+//       backdrop = <Backdrop click={this.backdropClickHandler}/>
+//     };
+
+//     return (
+//       <AppContext.Provider value={contextValue}>
+//         <main className="App" style={{height: '100%' }}>
+//           <Nav drawerClickHandler={this.drawerToggleClickHandler}/>
+//           <SideDrawer
+//             show={this.state.sideDrawerOpen}
+//             toggle={this.drawerToggleClickHandler}
+//           />
+//           {backdrop}
+//           <Switch>
+//             <>
+//               <div className='content' aria-live='polite'>
+//                 <Route
+//                   exact
+//                   path='/'
+//                   component={LandingPage}
+//                 />
+//                 <Route
+//                   exact
+//                   path='/signup'
+//                   component={SignUp}
+//                 />
+//                 <Route
+//                   exact
+//                   path='/login'
+//                   component={LogIn}
+//                 />
+//                 <Route
+//                   path='/browsebooks'
+//                   render={() => (
+//                     <BrowseBooks
+//                       books={this.state.books}
+//                       bookshelf={this.state.bookshelf}
+//                     />
+//                   )}
+//                 />
+//                 <Route
+//                   path='/bookshelf'
+//                   render={() => (
+//                     <Bookshelf
+//                       books={books}
+//                       bookshelf={this.state.bookshelf}
+//                       handleRemoveBook={this.handleRemoveBook}
+//                     />
+//                   )}
+//                 />
+//                 <Route
+//                   exact
+//                   path='/reviewform'
+//                   component={ReviewForm}
+//                 />
+//               </div>
+//             </>
+//           </Switch>
+//         </main>
+//       </AppContext.Provider>
+//     )
+//   }
+// }
+
+// export default App;
--- src/Bookshelf/Bookshelf.js
+++ src/Bookshelf/Bookshelf.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import config from '../config';
 import { Link } from 'react-router-dom';
-import BookshelfApiService from '../services/bookshelf-api-service';
+// import BookshelfApiService from '../services/bookshelf-api-service';
 import TokenService from '../services/token-service';
 import './Bookshelf.css';
 
@@ -15,14 +15,14 @@ class Bookshelf extends React.Component {
     }
   }
 
-  handleRemoveBook = (id) => {
-    BookshelfApiService.deleteBookshelfItem(id)
-    let array = [...this.state.bookshelf]
-    let updatedBookshelf = array.filter(bookshelfItem => {
-      return bookshelfItem.id !== id
-    })
-    this.setState({ bookshelf: updatedBookshelf })
-  };
+  // handleRemoveBook = (id) => {
+  //   BookshelfApiService.deleteBookshelfItem(id)
+  //   let array = [...this.state.bookshelf]
+  //   let updatedBookshelf = array.filter(bookshelfItem => {
+  //     return bookshelfItem.id !== id
+  //   })
+  //   this.setState({ bookshelf: updatedBookshelf })
+  // };
 
   componentDidMount() {
     fetch(`${config.API_ENDPOINT}/bookshelf`)
@@ -44,8 +44,6 @@ class Bookshelf extends React.Component {
         })
       })
   };
-
-
 
   render() {
     let userId = TokenService.getUserId();
@@ -80,7 +78,7 @@ class Bookshelf extends React.Component {
             <button
               className='delete-button'
               id={bookshelfItem.id}
-              onClick={() => this.handleRemoveBook(bookshelfItem.id)}
+              onClick={() => this.props.handleRemoveBook(bookshelfItem.id)}
             >
               Delete
             </button>
@@ -100,6 +98,126 @@ class Bookshelf extends React.Component {
 export default Bookshelf;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import config from '../config';
+// import { Link } from 'react-router-dom';
+// import BookshelfApiService from '../services/bookshelf-api-service';
+// import TokenService from '../services/token-service';
+// import './Bookshelf.css';
+
+// class Bookshelf extends React.Component {
+//   constructor(props) {
+//     super(props)
+
+//     this.state = {
+//       bookshelf: this.props.bookshelf,
+//       currentUserId: TokenService.getUserId()
+//     }
+//   }
+
+//   handleRemoveBook = (id) => {
+//     BookshelfApiService.deleteBookshelfItem(id)
+//     let array = [...this.state.bookshelf]
+//     let updatedBookshelf = array.filter(bookshelfItem => {
+//       return bookshelfItem.id !== id
+//     })
+//     this.setState({ bookshelf: updatedBookshelf })
+//   };
+
+//   componentDidMount() {
+//     fetch(`${config.API_ENDPOINT}/bookshelf`)
+//       .then((bookshelfRes) => {
+//         if (!bookshelfRes.ok) {
+//           throw new Error(bookshelfRes.statusText)
+//         }
+//         return bookshelfRes.json()
+//       })
+//       .then(data => {
+//         this.setState({
+//           bookshelf: data,
+//           error: null
+//         })
+//       })
+//       .catch(err => {
+//         this.setState({
+//           error: 'Sorry, could not get books at this time.'
+//         })
+//       })
+//   };
+
+
+
+//   render() {
+//     let userId = TokenService.getUserId();
+//     function filterByUserId(item) {
+//       return item.user_id === Number(userId)
+//     };
+//     const userBooks = this.state.bookshelf.filter(filterByUserId);
+//     const booksToDisplay = userBooks.map((bookshelfItem, i) => {
+//       return (
+//         <div key={i} className='displayed-books'>
+//           <h2>{bookshelfItem.books.title}</h2>
+//           <h3>Author: {bookshelfItem.books.author}</h3>
+//           <div>
+//             <img
+//               src={bookshelfItem.books.graphic}
+//               alt='book cover'
+//             />
+//           </div>
+//           <div>
+//             <Link to={{
+//               pathname: '/reviewform',
+//               state: {
+//                 bookshelfItem: bookshelfItem,
+//                 bookshelf: this.state.bookshelf
+//               }
+//             }}>
+//               <button className='add-review-button'>
+//                 View / Add Review
+//               </button>
+//             </Link>
+//             {' '}
+//             <button
+//               className='delete-button'
+//               id={bookshelfItem.id}
+//               onClick={() => this.handleRemoveBook(bookshelfItem.id)}
+//             >
+//               Delete
+//             </button>
+//           </div>
+//         </div>
+//       )
+//     });
+//     return (
+//       <div>
+//         <h1>Your Bookshelf</h1>
+//         {booksToDisplay}
+//       </div>
+//     )
+//   };
+// };
+
+// export default Bookshelf;
 
 
 

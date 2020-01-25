aaf5a2956ef7928417f93ab2710fc35d683b1d75
Maya Bowman <mayashady1@gmail.com>
2020-01-25 11:05:19 -0500

    working on test

--- src/LogIn/Login.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import LogIn from './LogIn'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<LogIn />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
